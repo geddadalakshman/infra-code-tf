495fc16007af0b68f6fecb034d4065edcf3690e5
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-31 22:28:58 +0530

    updated

---
+++ ec2/main.tf
@@ -0,0 +1,44 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "devops-ansible"
+  owners           = [ data.aws_caller_identity.current.account_id]
+}
+
+resource "aws_instance" "component" {
+  ami           = data.aws_ami.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.sg.id ]
+
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "all_traffic"
+  }
+}
+
+variable "instance_type" {}
+variable "component" {}
---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "instances" {
+  default = {
+    frontend = {
+      name = "frontend"
+      type = "t3.micro"
+    }
+    catalouge = {
+      name = "catalouge"
+      type = "t3.micro"
+    }
+  }
+}
