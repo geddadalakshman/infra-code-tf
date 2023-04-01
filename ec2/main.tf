9ca86ad185b8eda6f596416d50cfca0c2191dd83
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-01 22:54:12 +0530

    updated

--- ec2/main.tf
+++ ec2/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ami" {
 }
 
 resource "aws_instance" "component" {
-  ami           = data.aws_ami.ami
+  ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.sg.id ]
 
