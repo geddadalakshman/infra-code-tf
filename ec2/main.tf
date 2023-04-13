c51a0251b3d6b7ec41a76eb54479a471f52be50c
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-13 11:53:32 +0530

    updated

--- ec2/main.tf
+++ ec2/main.tf
@@ -57,64 +57,64 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.private_ip]
 }
 
-###IAM Policy creation
-resource "aws_iam_policy" "ssm-policy" {
-  name        = "${var.env}-${var.component}-ssm"
-  path        = "/"
-  description = "${var.env}-${var.component}-ssm"
-
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": [
-          "ssm:GetParameterHistory",
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter"
-        ],
-        "Resource": "arn:aws:ssm:us-east-1:820762291138:parameter/dev.frontend*"
-      },
-      {
-        "Sid": "VisualEditor1",
-        "Effect": "Allow",
-        "Action": [
-          "ssm:DescribeParameters",
-          "ssm:ListAssociations"
-        ],
-        "Resource": "*"
-      }
-    ]
-  })
-}
-
-#IAM role
-resource "aws_iam_role" "role" {
-  name = "${var.env}-${var.component}-role"
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "ec2.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-#IAM instance profile
-resource "aws_iam_instance_profile" "profile" {
-  name = "${var.env}-${var.component}-profile"
-  role = aws_iam_role.role.name
-}
-
-#IAM role_policy attachment
-resource "aws_iam_role_policy_attachment" "policy-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.ssm-policy.arn
-}
+####IAM Policy creation
+#resource "aws_iam_policy" "ssm-policy" {
+#  name        = "${var.env}-${var.component}-ssm"
+#  path        = "/"
+#  description = "${var.env}-${var.component}-ssm"
+#
+#  policy = jsonencode({
+#    "Version": "2012-10-17",
+#    "Statement": [
+#      {
+#        "Sid": "VisualEditor0",
+#        "Effect": "Allow",
+#        "Action": [
+#          "ssm:GetParameterHistory",
+#          "ssm:GetParametersByPath",
+#          "ssm:GetParameters",
+#          "ssm:GetParameter"
+#        ],
+#        "Resource": "arn:aws:ssm:us-east-1:820762291138:parameter/dev.frontend*"
+#      },
+#      {
+#        "Sid": "VisualEditor1",
+#        "Effect": "Allow",
+#        "Action": [
+#          "ssm:DescribeParameters",
+#          "ssm:ListAssociations"
+#        ],
+#        "Resource": "*"
+#      }
+#    ]
+#  })
+#}
+#
+##IAM role
+#resource "aws_iam_role" "role" {
+#  name = "${var.env}-${var.component}-role"
+#  assume_role_policy = jsonencode({
+#    "Version" : "2012-10-17",
+#    "Statement" : [
+#      {
+#        "Effect" : "Allow",
+#        "Principal" : {
+#          "Service" : "ec2.amazonaws.com"
+#        },
+#        "Action" : "sts:AssumeRole"
+#      }
+#    ]
+#  })
+#}
+#
+##IAM instance profile
+#resource "aws_iam_instance_profile" "profile" {
+#  name = "${var.env}-${var.component}-profile"
+#  role = aws_iam_role.role.name
+#}
+#
+##IAM role_policy attachment
+#resource "aws_iam_role_policy_attachment" "policy-attach" {
+#  role       = aws_iam_role.role.name
+#  policy_arn = aws_iam_policy.ssm-policy.arn
+#}
