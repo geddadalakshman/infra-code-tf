982c37c786219a81791bdf5823162c610c86c9eb
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-13 12:43:19 +0530

    updated

--- ec2/main.tf
+++ ec2/main.tf
@@ -75,7 +75,7 @@ resource "aws_iam_policy" "ssm-policy" {
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource": "arn:aws:ssm:us-east-1:820762291138:parameter/dev.frontend*"
+        "Resource": "arn:aws:ssm:us-east-1:820762291138:parameter/${var.env}.${var.component}*"
       },
       {
         "Sid": "VisualEditor1",
