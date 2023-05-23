f9787df040cd4024d07f076a57494995c2aab01e
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-05-23 16:39:04 +0530

    updated

--- ec2/data.tf
+++ ec2/data.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "devops-ansible"
+  name_regex       = "Centos-8-DevOps-Practice"
   owners           = [ data.aws_caller_identity.current.account_id]
 }
 
